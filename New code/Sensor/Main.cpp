65fb76bd2cce500296ce1730a61e975a79e2dbef
Zachary <zach6215@gmail.com>
2022-11-06 07:57:11 -0800

    Added code that trakcs when program is started. Used to test wittypi.

--- New code/Sensor/Main.cpp
+++ New code/Sensor/Main.cpp
@@ -1,9 +1,20 @@
-
+#include <iostream>
+#include <fstream>
+#include <ctime>
+using namespace std;
 
 
 int main()
 {
+    ofstream logStream;
+    time_t startTime = time(0);
+    char* startTimeStr = ctime(&startTime);
 
 
+    logStream.open("log.txt");
+    logStream << "Program started at: " << startTimeStr << endl;
+
+
+    logStream.close();
     return 0;
 }
