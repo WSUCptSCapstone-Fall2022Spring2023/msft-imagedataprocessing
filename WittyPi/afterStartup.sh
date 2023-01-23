c5846c07a43f48b5822db549b2543d7d1385a5d8
Zachary <zachary.c.hall@wsu.edu>
2023-01-23 05:16:45 -0800

    Moved afterStartup.sh contents to beforeShutdown.sh. This should allow for shorter on time.

--- WittyPi/afterStartup.sh
+++ WittyPi/afterStartup.sh
@@ -5,3 +5,20 @@
 # If you want to run your commands after boot, you can place them here.
 #
 
+NOW=`date +"%D %T"`
+
+FILE=/home/pi/Desktop/msft-imagedataprocessing/WittyPi/WittyPi_log.txt
+
+#Capture picture
+/home/pi/Desktop/msft-imagedataprocessing/AGICamCapture/capture /home/pi/AGICamImages 1 5 &
+
+#Wait until pictures are taken
+wait
+
+#Upload to database
+java -jar /home/pi/Desktop/msft-imagedataprocessing/AGICamUpload/build/libs/AGICamUpload-1.0-SNAPSHOT.jar /home/pi/AGICamImages 'mongodb+srv://<username>:<password>@agicam-store.dsxer1a.mongodb.net/?retryWrites=true&w=majority' agicam QCZgJ97ledg5cXbf
+
+#Wait until upload is done are taken
+wait
+
+printf "System powered off at %s\n" "$NOW" >> "$FILE"
